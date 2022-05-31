ce9ef9400bbda03b6d933e5b604ebd68f96da324
Lukas Hager <lghhager@gmail.com>
2022-05-30 19:26:52 -0700

    small analysis edits to alejandro analysis, including rest differential

--- midweek_analysis.R
+++ midweek_analysis.R
@@ -66,3 +66,42 @@ data_midweek %>%
   group_by(at_short_week) %>% 
   summarize(points_home_team = mean(points_home),
             points_away_team = mean(points_away))
+
+# Maybe if we look at rest *differential* as well, we can get some difference
+
+rest_days <- data_midweek %>% 
+  dplyr::select(date, season, home_team, away_team) %>% 
+  pivot_longer(home_team:away_team) %>% 
+  dplyr::select(-name) %>% 
+  arrange(value, date) %>% 
+  group_by(season) %>% 
+  mutate(rest_days = as.numeric(date - lag(date)))
+
+data_midweek %>% 
+  left_join(rest_days %>% 
+              rename(home_rest_days = rest_days),
+            by = c('date', 'season', 'home_team' = 'value')) %>% 
+  left_join(rest_days %>% 
+              rename(away_rest_days = rest_days),
+            by = c('date', 'season', 'away_team' = 'value')) %>% 
+  mutate(home_rest_diff = home_rest_days - away_rest_days,
+         away_rest_diff = away_rest_days - home_rest_days) %>% 
+  group_by(home_rest_diff) %>% 
+  summarise(points_home_team = mean(points_home),
+            points_away_team = mean(points_away),
+            n = n()) %>% 
+  ungroup() %>% 
+  filter(!is.na(home_rest_diff) & between(home_rest_diff,-21,21), n>=10) %>% 
+  ggplot(data = .) + 
+  geom_point(aes(x = home_rest_diff, y = points_home_team, size = n))
+
+# What is the distribution of 
+
+data_midweek %>% 
+  dplyr::select(date, season, home_team, away_team, midweek) %>% 
+  pivot_longer(home_team:away_team) %>% 
+  group_by(value, season) %>% 
+  summarise(total_midweek = sum(midweek)) %>% 
+  ungroup() %>% 
+  pull(total_midweek) %>% 
+  summary()
