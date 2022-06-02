83bf0f766da5d9b9fd66a29eb7a07a517b374986
AleO19 <59837029+AleO19@users.noreply.github.com>
2022-06-01 19:31:20 -0700

    + regression adjusting for team strength

--- midweek_analysis.R
+++ midweek_analysis.R
@@ -9,8 +9,8 @@
 
 rm(list = ls())
 {
-  library(lubridate)
   library(tidyverse)
+  library(lubridate)
 }
 
 
@@ -107,6 +107,8 @@ data_midweek %>%
   summary()
 
 
+
+
 # There seems to be a difference in the % of matches each team 
 # played midweek as host
 data_midweek %>% 
@@ -121,12 +123,15 @@ data_midweek %>%
   tally() %>%
   ungroup() %>% spread(key = midweek, value = n) %>% 
   mutate(percentage_mw = `1`/(`1` + `0`)*100) %>% 
-  arrange(desc(percentage_mw)) %>% View()
+  arrange(desc(percentage_mw)) %>% 
+  mutate(bin = c(rep(1, 5), ))
 
 
 # Midweek assignation does not seem to be randomly assigned after all 
 
 
+
+###### ------------ 03. REGRESSION MODELS ----------------------------
 # We are going to assume conditional independence between X (midweek games)
 # and the outcome Y (points)
 
@@ -140,19 +145,28 @@ data_midweek <- data_midweek %>%
     TRUE ~0
   ))
 
+
+# Check from all the midweek teams, how many are home and how many away
+# Objective: are they comparable?
+
 # We are going to define the set Z as acn, before_national_comps, after_national_comps, 
 # ln_capacity, distance and Holidays in order to estimate P(X|Z) and then apply IPW
 
 
+# Number of games during acn and intl tournaments!!!
 XZmodel <- glm(as.factor(midweek) ~ as.factor(acn) + as.factor(before_national_comps) + 
                  as.factor(after_national_comps) + 
-                 ln_capacity + distance + as.factor(Holidays) , 
+                 ln_capacity + distance + as.factor(Holidays) + as.factor(season) , 
                family = "binomial",
                data = data_midweek)
 
 summary(XZmodel)
 
-
+data_midweek %>% 
+  filter(midweek == 1) %>% 
+  group_by(month(date), season) %>% 
+  tally() %>%
+  spread(key = `month(date)`, value = n)
 # What to do with all the non-significant variables?
 # Should we do variable selection first?
 # When should we consider second order terms and interactions?
@@ -164,8 +178,146 @@ pX_Z <- ifelse(data_midweek$midweek == 0, 1 - predict(XZmodel, type = "response"
 
 data_midweek$w <- 1/pX_Z
 
+n_distinct(as.factor(data_midweek$w))
 
-# We now try to estimate the ACE using these weights 
+# We now try to estimate the ACE using IPW where 
+## Z = covariates used in the previous model 
+## X = 1 if the team played during midweek 
+## Yobs = 1 if the home team won 
+
+
+x <- data_midweek$midweek  
+yobs <- case_when(
+  data_midweek$fthg > data_midweek$ftag ~  3, 
+  data_midweek$fthg < data_midweek$ftag ~ 0,
+  TRUE ~ 1)
+
+# Regular ACE_IPW
+mean(x*yobs/pX_Z  - (1-x)*yobs/(1-pX_Z))
+
+# Modified IPW estimator
+
+sum(x*yobs/pX_Z)/sum(x/pX_Z) - sum((1-x)*yobs/(1-pX_Z))/sum((1-x)/(1-pX_Z))
+
+
+reg <- glm(yobs ~ x, weights = 1/pX_Z)
+summary(reg)
+
+# Let's take into account the strength of the teams
+
+data_og <- bind_rows(
+  lapply(list.files('Datasets',
+                    full.names = TRUE,
+                    pattern = '\\d{4}\\-\\d{2}\\.csv'),
+         read_csv,
+         col_types = cols())
+) %>% 
+  janitor::clean_names() %>% 
+  mutate(date = as.Date(date, '%d/%m/%y'))
+
+
+win_probabilities <- data_og %>% 
+  select(div:bsa) %>% 
+  pivot_longer(b365h:bsa) %>% 
+  mutate(pred_result = str_sub(name, -1),
+         bookmaker = str_sub(name, 1, -2),
+         value = 1/value) %>% 
+  select(-name) %>% 
+  pivot_wider(names_from = pred_result,
+              values_from = value) %>% 
+  filter(!is.na(h)) %>% 
+  mutate(sum_probs = h + d + a,
+         h = h/sum_probs,
+         d = d/sum_probs, 
+         a = a/sum_probs) %>% 
+  group_by(date, home_team, away_team) %>% 
+  summarize(h = mean(h),
+            a = mean(a),
+            d = mean(d)) %>% 
+  ungroup() %>% 
+  mutate(expected_points_ht = 3*h, 
+         expected_points_at = 3*a)
+
+
+data_midweek <- data_midweek %>% 
+  left_join(
+    win_probabilities, 
+    by = c("date", "home_team", "away_team")
+  )
+
+# We now do the rolling means of the expected points for all teams for their last 4 previous matches
+
+dm2 <- data_midweek %>% 
+  filter(!is.na(expected_points_ht)) %>% 
+  pivot_longer(home_team:away_team) %>% 
+  rename(team = value) %>% 
+  mutate(exp_points = if_else(name == "away_team", expected_points_at, 
+                              expected_points_ht)) %>% 
+  group_by(season, team) %>% 
+  arrange(date) %>% 
+  mutate(cumulative_exp_points = cumsum(exp_points), 
+         rm_exp_points = (cumulative_exp_points - lag(cumulative_exp_points, 4))/4) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = name, 
+              values_from = team) %>% 
+  select(date, season, home_team, away_team, rm_exp_points)
+
+
+data_midweek2 <- data_midweek %>% 
+  left_join(dm2 %>% select(-away_team) %>% 
+              filter(!is.na(home_team)), 
+            by = c("date", "season", "home_team")) %>% 
+  rename(rm_exp_points_ht = rm_exp_points) %>% 
+  left_join(dm2 %>% select(-home_team) %>% 
+              filter(!is.na(away_team)), 
+            by = c("date", "season", "away_team")) %>% 
+  rename(rm_exp_points_at = rm_exp_points) 
+
+
+data_midweek2 <- data_midweek2 %>% filter(!is.na(rm_exp_points_at), !is.na(rm_exp_points_ht)) %>% 
+  mutate(
+    diff_rm_exp_points = rm_exp_points_ht - rm_exp_points_at # This is our proxy for team strength 
+  )
+
+# Let's see if the number of matches make sense
+
+data_midweek2 %>% 
+  group_by(season) %>% 
+  tally() 
+
+# We make a regression adjusting for confounding 
+### Consider X = treatment (midweek games),
+###          Y = game won by home team 
+###          p(Z) = propensity score
+###          S = team strength
+
+# If we Assume E[Y|X, p(Z), S] = beta0 + beta1x + beta2p(Z) + beta3S
+# then beta1 is our ACE
+data_midweek2 <- data_midweek2 %>% 
+  mutate(y = if_else(fthg > ftag, 1, 0),
+         y2 = case_when(
+           fthg > ftag ~ 3, 
+           fthg == ftag ~ 1,
+           TRUE ~ 0
+         ),
+         pz = 1/w)
+
+reg_adjusted <- lm(y ~ midweek + pz + diff_rm_exp_points, 
+                   data = data_midweek2)
+summary(reg_adjusted)
+
+
+# So, apparently, playing midweek reduces the average number of games 
+# you win by 0.03
+
+# on points: 
+
+reg_adjusted2 <- lm(y2 ~ midweek + pz + diff_rm_exp_points, 
+                   data = data_midweek2)
+summary(reg_adjusted2)
+
+
+
 
 
 # We want to look at the point differential by home vs away
@@ -240,7 +392,7 @@ points_by_midweek <- points_by_midweek %>%
 ggplot(data = points_by_midweek) + 
   geom_col(aes(x = team, y = diff, fill =n_midweek)) + 
   theme(axis.text.x = element_text(angle = 90))
-  
+
 
 pivot_longer(home_team:points_away) %>% 
   mutate(team = str_extract(name, '(home)|(away)')) %>% 
